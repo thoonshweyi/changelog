eb20814cbae4e67e64b5f94081344376ef036ed5
Thoon She Yi <thoonlay779@gmail.com>
2025-08-19 16:02:07 +0630

    Lib

---
+++ resources/views/layouts/tailwindcentral/footer.blade.php
@@ -0,0 +1,15 @@
+
+        <!-- Lightbox2 css1 js1  -->
+        <script src="{{ asset('vendor/pro1/changelog/public/assets/libs/fancybox/fancybox.umd.js') }}" type="text/javascript"></script>
+
+        <script src="{{ asset('vendor/pro1/changelog/public//js/jquery.min.js') }}"></script>
+
+        <script type="text/javascript">
+            Fancybox.bind("[data-fancybox]", {
+            // Your custom options
+            });
+        </script>
+        @yield('scripts')
+
+    </body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Pro1\Changelog\Http\Controllers\ChangeLogsController;
 
-Route::group(['middleware' => ['auth']], function () {
+// Route::group(['middleware' => ['auth']], function () {
     Route::resource('changelogs', ChangeLogsController::class);
     Route::get("/changelogsfetchalldatas",[ChangeLogsController::class,"fetchalldatas"])->name("changelogs.fetchalldatas");
 
 
     Route::resource('whatsnews', WhatsNewsController::class);
 
-});
+// });
